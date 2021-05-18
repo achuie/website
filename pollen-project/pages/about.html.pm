bf21e9e920428320f3609d111982e01162d899ca
Andrew Huie <achuie@protonmail.com>
2021-05-18 21:30:14 +0900

    Relative style.

---
+++ pollen-project/pages/about.html.pm
@@ -0,0 +1,23 @@
+#lang pollen
+
+◊h1{About}
+
+All about me, for those interested.
+
+◊h2{Education}
+
+I graduated from Rice University with a BA in Computer Science in 2016. I was
+in Sid Richardson college, the best college. \\
+◊i{Death from Above.}
+
+◊h2{Work}
+
+I am a software engineer experienced in program design for research application
+software. I am currently at Ascent Robotics in Tokyo, Japan, where we try to
+make robots autonomous.
+
+◊h2{Interests}
+
+I fill my time with a few different pursuits including disc golf, origami,
+travel, skiing, photography, vidya, calligraphy, and of course keeping my
+programming in shape.
--- pollen-project/template.html.p
+++ pollen-project/template.html.p
@@ -2,8 +2,9 @@
 <head>
   <meta charset="UTF-8">
   ◊(define title (select 'h1 doc))
+  ◊(define style-path (if (regexp-match #rx"/" (symbol->string here)) ".." "."))
   <title>◊(if title title "Home") &ndash; AH</title>
-  <link rel="stylesheet" type="text/css" media="all" href="style.css" />
+  <link rel="stylesheet" type="text/css" media="all" href="◊|style-path|/style.css" />
 </head>
 ◊(define (body-with-nav)
   ◊body{
