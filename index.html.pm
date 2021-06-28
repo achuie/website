6083813ea186c48c63f0da6af20e5c6974daef0a
Andrew Huie <achuie@protonmail.com>
2021-06-28 16:08:10 +0900

    Small addition to welcome page.

--- index.html.pm
+++ index.html.pm
@@ -1,3 +1,6 @@
 #lang pollen
 
-◊h2[#:style "line-height: 2.15em"]{Welcome!}
+◊; Line height to match that of `h1` in the other pages.
+◊h2[#:style "line-height: 2.15em"]{Welcome}
+
+Feel free to poke around---there's not much here.
