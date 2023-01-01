5f2a6915400fdbca2d3863d596621ea1c3edbea0
achuie <achuie@protonmail.com>
2023-01-01 13:27:01 +0900

    First masonry layout experiment.

--- photography.html.pm
+++ photography.html.pm
@@ -2,4 +2,17 @@
 
 ◊h1{Photography}
 
-asdf
+\\
+
+◊div[#:class "masonry-layout"]{
+  ◊div[#:class "masonry-panel"]{
+    ◊div[#:class "masonry-panel__content"]{
+      ◊img[#:src "https://media.githubusercontent.com/media/achuie/achuie.github.io/master/images/portfolio/the_hermitage.jpg" #:width "95%"]{}
+    }
+  }
+  ◊div[#:class "masonry-panel"]{
+    ◊div[#:class "masonry-panel__content"]{
+      ◊img[#:src "https://media.githubusercontent.com/media/achuie/achuie.github.io/master/images/rock_lichen.jpg" #:width "95%"]{}
+    }
+  }
+}
