c7f7710b25a444edc414cde2111c89a2ec376d74
achuie <achuie@protonmail.com>
2023-07-05 04:20:57 +0900

    Remove project root dir prefix.

--- pollen.rkt
+++ pollen.rkt
@@ -1,9 +1,6 @@
 #lang racket/base
 
 (require racket/string
-         racket/system
-         racket/port
-         racket/list
          pollen/decode 
          pollen/unstable/typography
          txexpr)
@@ -31,14 +28,6 @@
       ,text
       ; viewBox: 0--16 square
       (svg ((xmlns "http://www.w3.org/2000/svg") (viewBox "-3 0 19 18") (width "0.4rem") (height "0.4rem") (fill "currentColor"))(path ((fill-rule "evenodd") (d "M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"))) (path ((fill-rule "evenodd") (d "M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"))))))
-
-;; Use a relative path for any project subdirectories.
-(define rooted-path-prefix
-  (if (ormap (λ (dir)
-                (string-contains? (path->string (current-project-root)) dir))
-             '("pages" "css" "posts"))
-    ".."
-    (current-project-root)))
 
 ;; Only convert a newline to a linebreak if the preceding line ends with "\\".
 (define (latex-linebreaker prev next)
