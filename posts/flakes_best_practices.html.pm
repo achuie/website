a32f8af3e8e8a825856382a967d42329624e4864
achuie <achuie@protonmail.com>
2023-07-18 01:03:51 +0900

    Get started.

---
+++ posts/flakes_best_practices.html.pm
@@ -0,0 +1,8 @@
+#lang pollen
+
+◊h1{What's the best way to write Nix Flakes?}
+
+As a new convert to the Church of Nix, jumping straight to new-fangled Flakes seems to be the
+thing to do. But everyone's got their own way of prescribing how best to structure them, and there
+isn't much consensus as Nix Flakes is still an evolving feature subject to change. So just for my
+sanity I'll try to inexhaustively enumerate and compare all the methods I've seen.
