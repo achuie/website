d2dc7996be725b969c661d781c35b08b797b0712
achuie <achuie@protonmail.com>
2023-07-19 01:00:30 +0900

    Outline.

--- posts/flakes_best_practices.html.pm
+++ posts/flakes_best_practices.html.pm
@@ -2,7 +2,19 @@
 
 ◊h1{What's the best way to write Nix Flakes?}
 
-As a new convert to the Church of Nix, jumping straight to new-fangled Flakes seems to be the
+As a new convert to the ◊em{Church of Nix}, jumping straight into new-fangled Flakes seems to be the
 thing to do. But everyone's got their own way of prescribing how best to structure them, and there
 isn't much consensus as Nix Flakes is still an evolving feature subject to change. So just for my
 sanity I'll try to inexhaustively enumerate and compare all the methods I've seen.
+
+◊h2{The Project}
+
+◊h2{Plain Jane}
+
+◊h2{Anti-Pattern}
+
+◊h2{Galaxy Brain}
+
+◊h2{Cannonball}
+
+◊h2{Goldilocks}
