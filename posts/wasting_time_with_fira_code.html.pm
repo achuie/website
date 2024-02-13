0cdec5351070b59b59ac1d512b7ebfc84ef0dd84
achuie <achuie@protonmail.com>
2024-02-14 01:08:49 +0900

    Switch to writing about customizing Fira Code.

---
+++ posts/wasting_time_with_fira_code.html.pm
@@ -0,0 +1,13 @@
+#lang pollen
+
+◊h1{Wasting Time with Fira Code}
+
+I spend a lot of time in the terminal. At home and at work, most of the tools I use are commandline, so I need a
+good-looking font, and Fira Code is second to none. It's an update to Fira Mono, the font Mozilla commissioned from
+Carrois Studios (from what I can tell). The letter forms are unique and stylish, without calling too much attention to
+themselves or becoming varied enough to cause fatigue. 
+
+However, the update also comes with ligatures. These are controversial, to say the least, and while some are intrusive
+and a bit obnoxious, some others are pretty and almost useful. Upon reflection, I realized the ones I like are all ones
+that do not mutate the shape of the individual composing glyphs, rather they simply adjust the kerning and x-height to
+fit the neighboring glyphs.
