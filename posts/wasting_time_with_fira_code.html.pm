965980bcfdf8a1f2564e77470f7337acf4fd409b
achuie <achuie@protonmail.com>
2024-02-15 19:10:50 +0900

    WIP need to add shell-cmd tag.

--- posts/wasting_time_with_fira_code.html.pm
+++ posts/wasting_time_with_fira_code.html.pm
@@ -11,3 +11,21 @@ However, the update also comes with ligatures. These are controversial, to say t
 and a bit obnoxious, some others are pretty and almost useful. Upon reflection, I realized the ones I like are all ones
 that do not mutate the shape of the individual composing glyphs, rather they simply adjust the kerning and x-height to
 fit the neighboring glyphs.
+
+◊; TODO bad ligature example
+◊; img[#:style "width: 6vmax; vertical-align: middle;"
+◊;     #:src "https://media.githubusercontent.com/media/achuie/achuie.github.io/master/images/columns.jpg"
+◊;     #:alt "Icon"]{} ◊br{}
+
+◊; TODO good ligature example
+◊; img[#:style "width: 6vmax; vertical-align: middle;"
+◊;     #:src "https://media.githubusercontent.com/media/achuie/achuie.github.io/master/images/columns.jpg"
+◊;     #:alt "Icon"]{} ◊br{}
+
+So below is a recount of my short endeavor to split these groups of ligatures and strip out the bad ones.
+
+◊h2{TL;DR}
+
+The code is ◊body-link[https://github.com/achuie/dotfiles/tree/master/nix-flakes/firacode]{here}, and can be built with
+the following command if one has Nix installed:
+◊shell-cmd{nix build github:achuie/dotfiles?dir=nix-flakes/firacode}
