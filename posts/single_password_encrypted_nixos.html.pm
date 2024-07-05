63d5f85e848f2b058f0f3a601217293a2a79e5c7
achuie <31836492+achuie@users.noreply.github.com>
2024-07-05 14:16:26 +0900

    More notes.

--- posts/single_password_encrypted_nixos.html.pm
+++ posts/single_password_encrypted_nixos.html.pm
@@ -2,16 +2,14 @@
 
 ◊h1{Single-Password Encrypted NixOS}
 
-I recently installed NixOS on my home workstation. It's root-on-BTRFS with home-on-ZFS, mainly because I didn't want to
-deal with swap on ZFS and the root SSD would have been a separate pool anyway, so I figured I might as well go with
-BTRFS+LUKS-on-LVM to try out all the new-to-me technologies at once. I searched extensively for guides and other
-troubleshooting threads prior to installing, and I found some pretty good ones
-
-◊h2{TL;DR}
-
-The code is ◊body-link[https://github.com/achuie/dotfiles/tree/master/nix-flakes/firacode]{here}, and can be built with
-the following command if one has Nix installed:
-◊shell-cmd{nix build github:achuie/dotfiles?dir=nix-flakes/firacode}
+I recently installed NixOS on my home workstation/server. I've only put /home on ZFS instead of going full root on ZFS,
+mainly because I didn't want to deal with swap on ZFS and the root SSD would have been the odd drive out in a ZFS mirror
+anyway, so I figured I might as well go with BTRFS+LUKS-on-LVM to try out all the new-to-me technologies at once. I
+searched extensively for guides and other troubleshooting threads prior to installing, and I found some pretty
+◊body-link[
+    "https://blog.kolaente.de/2021/11/installing-nixos-with-encrypted-btrfs-root-device-and-home-manager-from-start-to-finish/"
+]{good}
+◊body-link["https://gist.github.com/ladinu/bfebdd90a5afd45dec811296016b2a3f"]{ones}.
 
 
 ◊;;NOTES
@@ -30,3 +28,10 @@ https://jrs-s.net/2018/08/17/zfs-tuning-cheat-sheet/
 
 https://discourse.nixos.org/t/how-to-run-init-stage-2-manually/38625
 - info about stage 2 root path
+
+https://astrid.tech/2021/12/17/0/two-disk-encrypted-zfs/
+- guide with multiple zpools
+- some info about keylocation
+
+https://old.reddit.com/r/zfs/comments/bnvdco/zol_080_encryption_dont_encrypt_the_pool_root/
+- future work
