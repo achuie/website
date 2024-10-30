6f628d1dbcb65e46ad7c047bd7bccc66be45fd64
achuie <achuie@protonmail.com>
2024-10-30 23:59:48 +0900

    Update for plain code blocks.

--- posts/single_password_encrypted_nixos.html.pm
+++ posts/single_password_encrypted_nixos.html.pm
@@ -2,13 +2,15 @@
 
 ◊h1{Single-Password Encrypted NixOS}
 
-I recently installed NixOS on my home workstation/server. I've only put ◊code{/home} on ZFS instead of going full root
-on ZFS, mainly because I didn't want to deal with swap on ZFS and the root SSD would have been the odd drive out in a
-ZFS mirror anyway, so I figured I might as well go with BTRFS+LVM-on-LUKS to try out all the new-to-me technologies at
-once. I found some ◊body-link["https://astrid.tech/2021/12/17/0/two-disk-encrypted-zfs/"]{pretty} ◊body-link[
+I recently installed NixOS on my homelab. I found some
+◊body-link["https://astrid.tech/2021/12/17/0/two-disk-encrypted-zfs/"]{pretty} ◊body-link[
   "https://blog.kolaente.de/2021/11/installing-nixos-with-encrypted-btrfs-root-device-and-home-manager-from-start-to-finish/"
 ]{good} ◊body-link["https://gist.github.com/ladinu/bfebdd90a5afd45dec811296016b2a3f"]{guides}, but there were some steps
 that were different so I wanted to write down what I did for future reference.
+
+I've only put ◊code{/home} on ZFS instead of going full root on ZFS, mainly because I didn't want to deal with swap on
+ZFS and the root SSD would have been the odd drive out in a ZFS mirror anyway, so I figured I might as well go with
+BTRFS+LVM-on-LUKS to try out all the new-to-me technologies at once.
 
 ◊h2{Create the Installation Media}
 
@@ -24,11 +26,11 @@ conv=fsync oflag=direct status=progress
 Boot from the disk we just created. Ethernet should already be set up, but wifi requires extra steps:
 
 ◊code-block{
-# wpa_passphrase $SSID $PASSWORD >/etc/wpa_supplicant.conf \\
+# wpa_passphrase $SSID $PASSWORD >/etc/wpa_supplicant.conf
 # systemctl start wpa_supplicant
 }
 
-Identify your drives (◊code{fdisk -l}), and start with what will be the OS drive. Run ◊code{gdisk} on it, no partition
+Identify our drives (◊code{fdisk -l}), and start with what will be the OS drive. Run ◊code{gdisk} on it, no partition
 numbers since we'll be using the whole drive.
 
 ◊code-block{# gdisk /dev/$DISK}
@@ -50,7 +52,7 @@ keyfile for LUKS:
 # dd if=/dev/urandom of=./keyfile0.bin bs=1024 count=4
 }
 
-ZFS requires a thirty-two byte keyfile:
+and then a random key for ZFS, which requires a thirty-two byte keyfile:
 
 ◊code-block{
 # dd if=/dev/urandom of=./keyfile1.bin bs=32 count=1
@@ -63,25 +65,25 @@ ZFS requires a thirty-two byte keyfile:
 Let's set up the root partition. LUKS first; the password we set here will be the one needed at boot:
 
 ◊code-block{
-# cryptsetup luksFormat --type luks1 -c aes-xts-plain64 -s 256 -h sha512 /dev/${DISK}2 \\
-# cryptsetup luksAddKey /dev/${DISK}2 keyfile0.bin \\
+# cryptsetup luksFormat --type luks1 -c aes-xts-plain64 -s 256 -h sha512 /dev/${DISK}2
+# cryptsetup luksAddKey /dev/${DISK}2 keyfile0.bin
 # cryptsetup luksOpen /dev/${DISK}2 root --key-file keyfile0.bin
 }
 
 We need to use LUKS version 1 in order to work with GRUB. Also, we're using GRUB instead of systemd-boot because GRUB is
-able to work with an encrypted ◊code{/boot} (as long as ◊code{/boot/efi} is unencrypted). Now, we set up LVM:
+able to work with an encrypted ◊code{/boot} (as long as ◊code{/boot/efi} is unencrypted). Set up LVM with the following:
 
 ◊code-block{
-# pvcreate /dev/mapper/root \\
-# vgcreate vg /dev/mapper/root \\
-# lvcreate -L 8G -n swap vg \\
+# pvcreate /dev/mapper/root
+# vgcreate vg /dev/mapper/root
+# lvcreate -L 8G -n swap vg
 # lvcreate -l '100%FREE' -n root vg
 }
 
 Now we can format each logical volume. We'll start with swap, and activate it:
 
 ◊code-block{
-# mkswap /dev/mapper/vg-swap \\
+# mkswap /dev/mapper/vg-swap
 # swapon /dev/mapper/vg-swap
 }
 
@@ -99,14 +101,21 @@ And the EFI partition:
 
 ◊h3{Home Drive}
 
-First we need to identify our disks in a persistent way. We'll use ◊code{/dev/disk/by-id} for this, and store the IDs of
-the drives we want to use in our ZFS array for easy insertion into the zpool creation command. But since it maps
-partitions as well we need to filter a listing of the available drives. Starting with a bit of info about your drives,
-for instance I'm using two identical ten-terabyte Toshibas, we can make a spece-delimited list of their IDs with the
-following:
+First we need to identify our disks in a persistent way. We'll use ◊code{/dev/disk/by-id} for this. We can get around
+having to deal with the unique IDs with the following:
 
 ◊code-block{
 $ DISKS=$(ls /dev/disk/by-id/ata-TOSHIBA_* | grep -v 'part' | tr '\n' ' ')
+}
+
+Now we can assemble the ZFS options for our pool. I consulted this
+◊body-link["https://jrs-s.net/2018/08/17/zfs-tuning-cheat-sheet/"]{older guide}, which still seemed applicable, and
+created the pool as below:
+
+◊code-block{
+# zpool create -O encryption=on -O keyformat=raw -O keylocation=file:///mnt-root/etc/secrets/initrd/keyfile1.bin
+-O compression=zstd -O mountpoint=none -O xattr=sa -O acltype=posix -O atime=off -O secondarycache=none -o ashift=12
+ztank mirror $DISKS
 }
 
 
