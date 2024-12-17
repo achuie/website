6baca930c2f0caca2ece6eba07b8dcf4f685d1f3
achuie <31836492+achuie@users.noreply.github.com>
2024-12-17 18:15:18 +0900

    WIP List posts in reverse chronological order.

--- pages/transient-comprehension.html.pm
+++ pages/transient-comprehension.html.pm
@@ -1,7 +1,23 @@
 #lang pollen
 
+◊(require racket/string
+          racket/system
+          racket/port
+          racket/list
+          racket/path
+          racket/date)
+
 ◊h1{Transient Comprehension}
+
+\\
 
 ◊h4{Posts}
 
-TODO
+◊(map (λ (path) `(a ((href ,(path-element->string path))) (select 'h1 path)))
+      (sort (filter (λ (name) (define ext (path-get-extension name)) (and ext (bytes=? ext #".pm")))
+                    (directory-list (build-path ".." "posts")))
+            >
+            #:key (λ (post) (define seconds (select-from-metas
+                                                              'published
+                                                              (get-metas (build-path ".." "posts" post))))
+                    (if seconds (string->number seconds) 0))))
