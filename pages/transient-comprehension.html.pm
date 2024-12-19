7e80cf1b6aa403d9c4ea62bfef7df338a7721fde
achuie <31836492+achuie@users.noreply.github.com>
2024-12-19 17:51:14 +0900

    WIP Trying to splice list into doc, getting invalid txexpr.

--- pages/transient-comprehension.html.pm
+++ pages/transient-comprehension.html.pm
@@ -5,19 +5,29 @@
           racket/port
           racket/list
           racket/path
-          racket/date)
+          racket/date
+          pollen/core)
 
-◊h1{Transient Comprehension}
+◊h1{Project SPINLOCK}
 
 \\
 
 ◊h4{Posts}
 
-◊(map (λ (path) `(a ((href ,(path-element->string path))) (select 'h1 path)))
-      (sort (filter (λ (name) (define ext (path-get-extension name)) (and ext (bytes=? ext #".pm")))
-                    (directory-list (build-path ".." "posts")))
-            >
-            #:key (λ (post) (define seconds (select-from-metas
-                                                              'published
-                                                              (get-metas (build-path ".." "posts" post))))
-                    (if seconds (string->number seconds) 0))))
+◊(define post-links (map (λ (path) `(a ((href ,(string-trim (path-element->string path) ".pm"))) (select 'h1 path)))
+       (sort (filter (λ (name) (define ext (path-get-extension name)) (and ext (bytes=? ext #".pm")))
+                     (directory-list (build-path ".." "posts")))
+             >
+             #:key (λ (post)
+                     (define published (select-from-metas 'published (get-metas (build-path ".." "posts" post))))
+                     (define seconds (if published
+                                       (apply
+                                         find-seconds
+                                         (append (list 0 0 0)
+                                                 (map (λ (s) (string->number s))
+                                                      (string-split published))
+                                                 (list #f)))
+                                       #f))
+                     (if seconds seconds 0)))))
+
+◊ul{◊(@ (map (λ (link) `(li ,link)) post-links))}
