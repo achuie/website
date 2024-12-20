bb88fb0fc4524ec1f4725bacd382a0f0f1812d69
achuie <31836492+achuie@users.noreply.github.com>
2024-12-20 13:22:32 +0900

    Use formatter.

--- pages/transient-comprehension.html.pm
+++ pages/transient-comprehension.html.pm
@@ -9,25 +9,27 @@
 
 ◊(define (get-published-timestamp pollen-srcfile)
          (define published (select-from-metas 'published (get-metas (build-path posts-path pollen-srcfile))))
-         (define seconds (if published
-                           (apply find-seconds
-                                  (append (list 0 0 0)
-                                          (map (λ (s) (string->number s)) (string-split published))
-                                          (list #f)))
-                           #f))
+         (define seconds
+           (if published
+             (apply
+               find-seconds
+               (append (list 0 0 0) (map (λ (s) (string->number s)) (string-split published)) (list #f)))
+             #f))
          (if seconds seconds 0))
 ◊(define posts-path (build-path ".." "posts"))
 ◊(define post-links
          (map (λ (sorted-pollen-srcfile)
                 (define path-to-srcfile (build-path posts-path sorted-pollen-srcfile))
                 (define srcfile-published-ts (get-published-timestamp sorted-pollen-srcfile))
-                `(a ((class "bodylink")
-                     (title ,(if (> srcfile-published-ts 0)
-                               (date->string (seconds->date srcfile-published-ts))
-                               "unpublished"))
-                     (href ,(string-trim (path->string path-to-srcfile) ".pm")))
-                    ,(string-append (if (> srcfile-published-ts 0) "" "(unpublished) ") (select 'h1 path-to-srcfile))))
-              (sort (filter (λ (name) (define ext (path-get-extension name)) (and ext (bytes=? ext #".pm")))
+                `(a ((class "bodylink") (title ,(if (> srcfile-published-ts 0)
+                                                  (date->string (seconds->date srcfile-published-ts))
+                                                  "unpublished"))
+                                        (href ,(string-trim (path->string path-to-srcfile) ".pm")))
+                    ,(string-append (if (> srcfile-published-ts 0) "" "(unpublished) ")
+                                    (select 'h1 path-to-srcfile))))
+              (sort (filter (λ (name)
+                              (define ext (path-get-extension name))
+                              (and ext (bytes=? ext #".pm")))
                             (directory-list (build-path posts-path)))
                     >
                     #:key get-published-timestamp)))
