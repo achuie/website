b3917f886c24119f4b9e292dba45dd9823068b99
achuie <31836492+achuie@users.noreply.github.com>
2025-01-17 18:57:01 +0900

    Use relative path.

--- js/fill_image.js
+++ js/fill_image.js
@@ -3,13 +3,11 @@ var query = window.location.search.substring(1).split("&");
 for (var i = 0, max = query.length; i < max; i++) {
   // check for trailing & with no param
   if (query[i] === "")
-  continue;
+    continue;
 
   var param = query[i].split("=");
   GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
 }
 var imageToFill = document.getElementById("fillIn")
-imageToFill.src =
-  "https://media.githubusercontent.com/media/achuie/achuie.github.io/master/images/portfolio"
-  .concat('/', GET.Viewing_Image);
+imageToFill.src = "../images/portfolio".concat('/', GET.Viewing_Image);
 imageToFill.alt = GET.Viewing_Image;
