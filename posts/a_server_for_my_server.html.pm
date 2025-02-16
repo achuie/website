2128e0e7f8120dfa2b0fe3da493931fcb5488a63
achuie <achuie@protonmail.com>
2025-02-17 00:48:58 +0900

    Start writing.

---
+++ posts/a_server_for_my_server.html.pm
@@ -0,0 +1,12 @@
+#lang pollen
+
+◊(define-meta published "?? 02 2025")
+
+◊h1{Setting Up a Server for My Server}
+
+So I set up my homelab, but now I have to make it accessible from outside my apartment. Normally this is trivially done
+with port forwarding rules on one's router, but my apartment building has a centralized, building-wide router from our
+internet provider that I don't have control over. I could probably call and ask them to assign me a static IP address
+and forward a port for met, but knowing Japan I bet they either don't allow it or hide it behind a form I'd have to
+wait for them to snailmail to me, fill out and attach some residency record procured from the ward office (300 yen),
+then snailmail back to them.
