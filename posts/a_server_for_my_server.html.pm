3925d41177759883d17cc6a319c6b085bd003d3f
achuie <achuie@protonmail.com>
2025-02-20 00:11:41 +0900

    Writing progress.

--- posts/a_server_for_my_server.html.pm
+++ posts/a_server_for_my_server.html.pm
@@ -6,7 +6,8 @@
 
 So I set up my homelab, but now I have to make it accessible from outside my apartment. Normally this is trivially done
 with port forwarding rules on one's router, but my apartment building has a centralized, building-wide router from our
-internet provider that I don't have control over. I could probably call and ask them to assign me a static IP address
-and forward a port for met, but knowing Japan I bet they either don't allow it or hide it behind a form I'd have to
-wait for them to snailmail to me, fill out and attach some residency record procured from the ward office (300 yen),
-then snailmail back to them.
+internet provider that I don't have control over. I could probably call and ask them about it, but knowing Japan I bet
+they either don't allow it or hide it behind so many bureaucratic processes and forms as to render it not worth the
+while.
+
+There is an alternative, however: SSH tunneling. Specifically, reverse SSH tunneling.
